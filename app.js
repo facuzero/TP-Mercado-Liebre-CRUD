3f6760e66d13109c51104ddcb5b11ce3e42b5725
facuzero <facuzero_95@hotmail.com>
2021-11-06 17:53:30 -0300

    agrego animacion al camion de envio

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ app.use(express.static('public'));
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname,'/views/home.html'))
 })
+app.get('/login.html', (req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/login.html'))
+})
+app.get('/register.html', (req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/register.html'))
+})
 
 app.listen(PORT, () => console.log(`
 Servidor levantado en el puerto ${PORT}
